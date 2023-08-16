74efebcdcc9bca9db568b2e0fd63af59bafd4338
Maddie Dew <maddiedew12@gmail.com>
2023-08-16 09:41:47 -0400

    Initial Commit

---
+++ myFirstApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  myFirstApp
+//
+//  Created by scholar on 8/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to New York City!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.pink)
+        
+            
+            Image("RubberDuckyNYC2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                
+        }
+        .padding(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by scholar on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
